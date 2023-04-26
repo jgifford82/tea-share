babb391d9cdac5c29b560e198214d67eefd8429d
Jennifer Gifford <jgifford82@gmail.com>
2023-04-25 21:33:42 -0600

    add rescue_from in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  # this is for error handling in SignUp 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  
 end
