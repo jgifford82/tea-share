93531e929abbea226949a1d39bc09aead4c28938
Jennifer Gifford <jgifford82@gmail.com>
2023-04-25 21:32:51 -0600

    add error handling in users controller create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    # POST new user (sign up)
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        if user.valid?
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    # permit only the parameters that we want to use. prevents users from updating attributes they shouldn't have access to, making it more secure. 
+    def user_params
+      params.permit(:username, :password, :password_confirmation)
+    end
 end
