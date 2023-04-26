e2bfb5111ab04313ddbd8c6b832a7f301a8846e4
Jennifer Gifford <jgifford82@gmail.com>
2023-04-25 21:39:20 -0600

    define render_unprocessable_entity_response in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::API
 
   # this is for error handling in SignUp 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-  
+
+  # this is for error handling in SignUp 
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
