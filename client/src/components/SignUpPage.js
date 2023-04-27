0331d64409bf3da2fb94bdefb29e7017e3b11d94
Jennifer Gifford <jgifford82@gmail.com>
2023-04-26 20:53:21 -0600

    add signUpForm & button conditional rendering

--- client/src/components/SignUpPage.js
+++ client/src/components/SignUpPage.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import SignUpForm from "./SignUpForm";
 
 const SignUpPage = () => {
+  const [showSignUpForm, setShowSignUpForm] = useState(false);
+
+  // button sets showSignUpForm state to true so it affects conditional rendering below
+  const button = (
+    <button onClick={() => setShowSignUpForm(true)}>Sign Up</button>
+  );
+  const noAccount = <div>Don't have an account? {button}</div>;
+
   return (
     <div>
-      <SignUpForm />
+      {/* conditional rendering of text & button depending on whether or not a user is signed up */}
+      {noAccount}
+      {/* conditional rendering of sign up form to appear when a user clicks the button */}
+      {showSignUpForm ? (
+        <SignUpForm setShowSignUpForm={setShowSignUpForm} />
+      ) : null}
     </div>
   );
 };
