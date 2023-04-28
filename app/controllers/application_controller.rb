5c0fdbc65dcb85dcd647410a7e4615086e715e0b
Jennifer Gifford <jgifford82@gmail.com>
2023-04-27 21:15:22 -0600

    add authorize method & authorize before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,16 @@ class ApplicationController < ActionController::API
   # this is for error handling in SignUp 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
+  # checks that users are logged in (authorized) before an action
+  before_action :authorize
+
+  # users must be logged in otherwise they'll get the unauthorized message
+  def authorize
+    return render json: { error: "Not Authorized" }, status: :unauthorized unless session.include? :user_id
+  end
+
+  private
+  
   # this is for error handling in SignUp 
   def render_unprocessable_entity_response(exception)
     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
