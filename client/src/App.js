dbf3c315c212de5daaf4d5cbfccc9b7f225e852c
Jennifer Gifford <jgifford82@gmail.com>
2023-04-27 20:42:46 -0600

    import NavNar in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
