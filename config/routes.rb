58c96c5005200a4aa817b00fa340aa3b98327182
Jennifer Gifford <jgifford82@gmail.com>
2023-04-29 13:08:05 -0600

    add get route for auto login in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
+  get "/auth", to: "users#show"
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
