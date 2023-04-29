e1485d6531fe7443bf5afd04dfbc5619b79ff47a
Jennifer Gifford <jgifford82@gmail.com>
2023-04-29 17:44:16 -0600

    set up teas fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
-import { useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { UserContext } from "./context/user";
+import { TeasContext } from "./context/teas";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import TeasList from "./components/TeasList";
 
 function App() {
   const { user, setUser } = useContext(UserContext);
+  const { teas, setTeas } = useContext(TeasContext);
+  const [loading, setLoading] = useState(true);
+
+  // Fetches teas data (containing reviews & category for each teas) from backend server & sets state with that data.
+  useEffect(() => {
+    fetch("/teas")
+      .then((r) => r.json())
+      // .then((data) => console.log(data));
+      // .then((data) => setTeas(data));
+      .then((data) => {
+        setTeas(data);
+        setLoading(false);
+      });
+  }, [setTeas]);
+
+  console.log(teas);
 
   // don't need setUser in dependency array, but added it in to clear warning on browser console. removing dependency array led to continuous fetches.
   useEffect(() => {
@@ -25,8 +43,11 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* !loading && checks if loading is false before rendering the TeasList component so it renders when teas data finishes loading */}
           {/* if user is truthy, && operator returns the route so a user that's logged in can see the teas */}
-          {user && <Route path="/teas" />}
+          {!loading && user && (
+            <Route path="/teas" element={<TeasList teas={teas} />} />
+          )}
         </Routes>
       </Router>
     </div>
