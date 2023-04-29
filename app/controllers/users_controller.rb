831acd4904c523348c19f217d1b28baf8dd3ec62
Jennifer Gifford <jgifford82@gmail.com>
2023-04-29 13:08:27 -0600

    add show method in users controller for auto login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,17 @@ class UsersController < ApplicationController
         end
     end
 
+    # GET user if the user's id is in params, otherwise get the logged in user (the user_id saved in session for auto login)
+    # json response includes all associated data for the logged in user (reviews and teas). this allows the logged in user to see all their reviews on My Reviews page without refreshing, even if they just posted a new review
+    def show
+        if params[:id]
+            user = User.find(params[:id])
+        else 
+            user = User.find_by!(id: session[:user_id])
+        end
+        render json: user, include: "*", status: 200
+    end
+
     private
 
     # permit only the parameters that we want to use. prevents users from updating attributes they shouldn't have access to, making it more secure. 
