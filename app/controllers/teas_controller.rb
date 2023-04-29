3b5bf731b092ed0fbfdbc55b943ab7a4078d8634
Jennifer Gifford <jgifford82@gmail.com>
2023-04-29 16:12:05 -0600

    get teas list alphabetically by name

--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -1,7 +1,11 @@
 class TeasController < ApplicationController
+    skip_before_action :authorize, only: [:index]
+
+    #including reviews & reviews.user in #index & #show allows access to deeply nested user data. without it, ActiveModel Serializer only nests associations 1 level deep. this also limits the user attributes to those in user serializer.
+
     # GET all teas alphabetically by title regardless of capitalization
     def index
-        tea = Tea.all
-        render json: tea
+        tea = Tea.order('lower(name)').all
+        render json: tea, include: ['reviews', 'reviews.user']
     end
 end
