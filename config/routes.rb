b640c794760a695d956402e9ac177650ddc1c90a
Jennifer Gifford <jgifford82@gmail.com>
2023-04-29 15:38:05 -0600

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #routing logic moved from bottom to top, otherwise the website displays json tea list when the page is manually refreshed in production
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
