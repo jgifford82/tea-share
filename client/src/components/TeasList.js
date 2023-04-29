51c4da20f8073f5e3085ee9b4b20d9db9e519a0b
Jennifer Gifford <jgifford82@gmail.com>
2023-04-29 17:41:11 -0600

    create TeasList component & display teas

---
+++ client/src/components/TeasList.js
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { TeasContext } from "../context/teas";
+
+const TeasList = () => {
+  const { teas } = useContext(TeasContext);
+
+  // conditional rendering to check whether the teas array exists before rendering it so the page doesn't crash if manually refreshed
+  if (!teas) {
+    return <div>Loading teas...</div>;
+  }
+
+  const renderTeas = teas.map((tea) => (
+    <ul key={tea.id}>
+      <em>
+        <Link to={`/teas/${tea.id}`}>{tea.name}</Link>
+      </em>{" "}
+      <br></br>
+      <strong>Blend:</strong> <br></br>
+      {tea.blend}
+      <br></br>
+      <br></br>
+      <strong>Category:</strong> <br></br>
+      {tea.category.name}
+    </ul>
+  ));
+  return (
+    <div>
+      <h1>Tea</h1>
+      {renderTeas}
+    </div>
+  );
+};
+
+export default TeasList;
