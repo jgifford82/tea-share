97e618121ee0996e8cdb7541b0533e0226fb80ae
Jennifer Gifford <jgifford82@gmail.com>
2023-04-29 13:10:42 -0600

    set up auth fetch in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
+import { useContext, useEffect } from "react";
+import { UserContext } from "./context/user";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const { setUser } = useContext(UserContext);
+
+  // don't need setUser in dependency array, but added it in to clear warning on browser console. removing dependency array led to continuous fetches.
+  useEffect(() => {
+    // auto-login
+    fetch("/auth").then((r) => {
+      if (r.ok) {
+        // r.json().then((user) => console.log(user));
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, [setUser]);
+
   return (
     <div className="App">
       <Router>
