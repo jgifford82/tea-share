68fcfe965003503034d594782bdff2c2b73fd4e0
Jennifer Gifford <jgifford82@gmail.com>
2023-04-29 15:27:52 -0600

    move routing logic to top of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
   resources :categories, only: [:index]
   resources :reviews, only: [:index]
   resources :teas, only: [:index]
@@ -9,8 +12,5 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   get "/auth", to: "users#show"
   delete "/logout", to: "sessions#destroy"
-  
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
