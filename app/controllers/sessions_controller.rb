83559279b95e5b2a6bb0ff20ffc608082195c23a
Jennifer Gifford <jgifford82@gmail.com>
2023-04-29 14:45:28 -0600

    add destroy method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,18 @@ class SessionsController < ApplicationController
     
     # POST login unless username or password are invalid
     def create
-    user = User.find_by(username: params[:username])
-    if user&.authenticate(params[:password])
-        session[:user_id] = user.id
-        render json: user, status: :created
-    else
-        render json: { error: "Invalid username or password" }, status: :unauthorized
-    end  
-end
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { error: "Invalid username or password" }, status: :unauthorized
+        end  
+    end
+
+    # DELETE logout 
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
 end
