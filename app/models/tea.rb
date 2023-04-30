9c1fd8eccba8d308b25e1abe496b92f6b342a438
Jennifer Gifford <jgifford82@gmail.com>
2023-04-30 13:50:38 -0600

    set up tea model validations

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -3,4 +3,13 @@ class Tea < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
     belongs_to :category
+
+    # makes sure name, blend, caffeine_level & category_id exists 
+    # name is unique regardless of capitalization
+    # blend has at least 10 characters
+    # caffeine_level must be an integer between 1-5
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
+    validates :blend, presence: true, length: { minimum: 10 }
+    validates :caffeine_level, presence: true, inclusion: { in: 1..5, message: "must be between 1 and 5" }
+    validates :category_id, presence: true
 end
