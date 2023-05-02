c0836bbaddd100cebde3c210e3d1dc16efe9b7ff
Jennifer Gifford <jgifford82@gmail.com>
2023-05-01 19:44:32 -0600

    set up add new tea handler in TeasContext

--- client/src/context/teas.js
+++ client/src/context/teas.js
@@ -9,9 +9,15 @@ function TeasProvider({ children }) {
   // Rather than having the teas variable as state in App component, we can set up the context value to be stateful.
   const [teas, setTeas] = useState(null);
 
+  // Updates state responsible for rendering teas when new tea is added, which displays new tea at the top of the list.
+  function addTea(newTea) {
+    console.log("In TeasList:", newTea);
+    setTeas((prevTeas) => [newTea, ...prevTeas]);
+  }
+
   return (
     // the value prop of the provider will be our context data. this value will be available to child components of this provider
-    <TeasContext.Provider value={{ teas, setTeas }}>
+    <TeasContext.Provider value={{ teas, setTeas, addTea }}>
       {children}
     </TeasContext.Provider>
   );
