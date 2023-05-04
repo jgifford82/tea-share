038819d3db6c037bf2ca24f643fad0db74b3a556
Jennifer Gifford <jgifford82@gmail.com>
2023-05-03 19:53:56 -0600

    add has_many reviews in tea serializer

--- app/serializers/tea_serializer.rb
+++ app/serializers/tea_serializer.rb
@@ -1,6 +1,9 @@
 class TeaSerializer < ActiveModel::Serializer
   attributes :id, :name, :blend, :caffeine_level, :user_id, :category_id
 
+  # access the reviews for each tea
+  has_many :reviews
+
   # access the tea category
   has_one :category
 end
