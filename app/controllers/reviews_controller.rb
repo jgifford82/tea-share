e492ab25350470da2cf9e14523d92bd4beea5cd3
Jennifer Gifford <jgifford82@gmail.com>
2023-05-08 19:22:08 -0600

    add create method in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,4 +11,29 @@ class ReviewsController < ApplicationController
         end
         render json: reviews
     end
+
+    # POST 1 new review per tea if a user is logged in
+    # Find tea by id in params & build a new review for that tea if a review by the current user doesnt already exist for that tea  
+    # Set the user for that review to the currently logged in user found in the session
+    # Save the review & return it as a response
+    def create
+        tea = Tea.find(params[:tea_id])
+        if tea.reviews.where(user_id: session[:user_id]).exists?
+          render json: { error: "You can only create one review per tea" }, status: :unprocessable_entity
+        else
+          review = tea.reviews.build(review_params)
+          review.user = User.find(session[:user_id])
+          if review.save
+            render json: review, status: :created
+          else
+            render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
+          end
+        end
+      end
+
+private
+
+    def review_params
+        params.require(:review).permit(:comment, :rating)
+    end
 end
