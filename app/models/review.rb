5c79cd327b1f4e9fc3c40251259aac25f3b340fa
Jennifer Gifford <jgifford82@gmail.com>
2023-05-08 19:22:42 -0600

    add validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,7 @@ class Review < ApplicationRecord
     # belongs_to must be singular for the macro to work properly, otherwise the inferred class will be wrongly pluralized too.
     belongs_to :tea
     belongs_to :user
+
+    validates :comment, presence: true, length: { minimum: 2 }
+    validates :rating, presence: true, inclusion: { in: 1..5, message: "must be between 1 and 5" }
 end
