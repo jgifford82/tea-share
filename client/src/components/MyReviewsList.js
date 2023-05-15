f4ade93f3e9711657c9547ac3234e685fd3cba80
Jennifer Gifford <jgifford82@gmail.com>
2023-05-15 15:15:58 -0600

    set up delete fetch request in MyReviewsList

--- client/src/components/MyReviewsList.js
+++ client/src/components/MyReviewsList.js
@@ -1,9 +1,40 @@
 import MyReviewsCard from "./MyReviewsCard";
+import { useState, useContext } from "react";
+import { TeasContext } from "../context/teas";
 
 const MyReviewsList = () => {
+  // use TeasContext to update teas & user state
+  const { deleteReview } = useContext(TeasContext);
+
+  const [errors, setErrors] = useState([]);
+
+  function handleDeleteClick(e, review) {
+    // tea & review id's taken from the review object argument passed into the function
+    const teaId = review.tea_id;
+    const reviewId = review.id;
+
+    fetch(`/teas/${teaId}/reviews/${reviewId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((r) => {
+      if (r.ok) {
+        setErrors([]);
+        // r.json().then((data) => console.log(data));
+        r.json().then((deletedReview) => deleteReview(deletedReview));
+      } else {
+        r.json().then((err) => console.log(err));
+        // r.json().then((err) => setErrors(err.error));
+      }
+    });
+  }
+
   return (
     <div>
-      <MyReviewsCard />
+      {/* if there's an error, display it in red */}
+      {errors ? <h3 style={{ color: "red" }}>{errors}</h3> : null}
+      <MyReviewsCard onDeleteClick={handleDeleteClick} />
     </div>
   );
 };
