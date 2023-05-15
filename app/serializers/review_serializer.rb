b210bd7f7e76d19976cd28868d42f7b3337be2fd
Jennifer Gifford <jgifford82@gmail.com>
2023-05-15 14:10:36 -0600

    add tea attribute to review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,8 +1,12 @@
 class ReviewSerializer < ActiveModel::Serializer
   # ActiveModel::Serializer gem enables customization of JSON to be rendered without sacrificing Rails principles of "convention over configuration" and separation of concerns.
   # list of attributes that we want to be included in JSON rendered by controller methods
-  attributes :id, :comment, :rating, :user_id, :tea_id
+  attributes :id, :comment, :rating, :user_id, :tea_id, :tea
 
   #belongs_to along with user attribute above returns user data in json. access username associated with each review.
   belongs_to :user
+
+  # when a new review is posted, the json response includes the new review's associated tea name. that way, it's available to display in the MyReviews page without having to refresh the page.
+  belongs_to :tea
+  # , except: [:created_at, :updated_at]
 end
