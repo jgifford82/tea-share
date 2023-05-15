2d2145ad166c18d292cae1321b80c3cd73995e86
Jennifer Gifford <jgifford82@gmail.com>
2023-05-15 17:28:35 -0600

    remove unused reviews resource in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
   resources :categories, only: [:index]
-  resources :reviews, only: [:index]
   resources :teas, only: [:index, :show, :create] do
     # nested resource for reviews
     # all 5 RESTful routes are used, no need for only:
