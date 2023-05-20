260b2e446ea596dfe84b9c5ff2d45fd52f3265ab
Jennifer Gifford <jgifford82@gmail.com>
2023-05-20 15:05:48 -0600

    add conditional edit rendering in MyReviewsCard

--- client/src/components/MyReviewsCard.js
+++ client/src/components/MyReviewsCard.js
@@ -1,10 +1,25 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../context/user";
+import MyReviewsEditForm from "./MyReviewsEditForm";
 
 const MyReviewsCard = ({ onDeleteClick }) => {
   // user variable from UserContext allows access to the list of logged in user's teas & reviews
   const { user } = useContext(UserContext);
-  console.log(user);
+  // console.log(user);
+
+  // keep track of the review object that is being edited
+  const [editingReview, setEditingReview] = useState(null);
+
+  // set the editingReview state to the review object that was clicked, or null if the same edit button is clicked again so the edit form is no longer displayed.
+  function handleEditClick(e, review) {
+    // console.log("clicked");
+    if (editingReview === review) {
+      setEditingReview(null);
+    } else {
+      setEditingReview(review);
+    }
+    // console.log(editingReview);
+  }
 
   // conditional rendering to check whether the user array exists before rendering it so the page doesn't crash if manually refreshed
   if (!user) {
@@ -17,8 +32,31 @@ const MyReviewsCard = ({ onDeleteClick }) => {
       <div key={review.id}>
         <h3>{review.tea.name}</h3>
         <button onClick={(e) => onDeleteClick(e, review)}>X</button>{" "}
-        {/* <button onClick={(e) => handleEditClick(e, review)}>Edit</button>{" "} */}
+        <button onClick={(e) => handleEditClick(e, review)}>Edit</button>{" "}
         <span>{review.comment}</span>
+        <br></br>
+        <strong>Rating:</strong> {review.rating}
+      </div>
+    );
+  });
+
+  const renderEditUserReviewedTeas = user.reviews.map((review) => {
+    return (
+      <div key={review.id}>
+        <h3>{review.tea.name}</h3>
+        <button onClick={(e) => onDeleteClick(e, review)}>X</button>{" "}
+        <button onClick={(e) => handleEditClick(e, review)}>Edit</button>
+        {editingReview && editingReview.id === review.id ? (
+          <MyReviewsEditForm
+            review={editingReview}
+            onEditClick={handleEditClick}
+          />
+        ) : (
+          <span>
+            {review.comment}
+            <strong>Rating:</strong> {review.rating}
+          </span>
+        )}
       </div>
     );
   });
@@ -26,7 +64,12 @@ const MyReviewsCard = ({ onDeleteClick }) => {
   return (
     <div>
       My Reviews:
-      <>{renderUserReviewedTeas}</>
+      {/* conditional rendering shows edit form when editingReview state is set to a review object */}
+      {editingReview ? (
+        <>{renderEditUserReviewedTeas}</>
+      ) : (
+        <>{renderUserReviewedTeas}</>
+      )}
     </div>
   );
 };
